36c896ad7f822fc9a5a73f32144f857685a12aeb
Karandeep Randhawa <karandeeprandhawa27@gmail.com>
2024-01-29 00:06:23 +0530

    Admin add/update/delete admin,vendor

--- kaaya_login/serializers.py
+++ kaaya_login/serializers.py
@@ -5,3 +5,8 @@ class AddLoginDetails(serializers.ModelSerializer):
     class Meta:
         model = TblKaayaLogin
         fields = ['id', 'username', 'password', 'usertype','is_email_verified','is_phone_verified','created_date']
+
+class DeleteLoginDetails(serializers.ModelSerializer):
+    class Meta:
+        model = TblKaayaLogin
+        fields = ['id', 'is_active','is_deleted','modified_date','modified_by']
--- kaayaa_admin/views.py
+++ kaayaa_admin/views.py
@@ -9,6 +9,12 @@ from django.utils import timezone
 from kaaya_login.serializers import AddLoginDetails
 from kaayaa_vendor.models import TblKaayaVendorDetails
 from kaayaa_vendor.serializers import GetVendorDetails
+from .models import TblKaayaAdminDetails
+from .serializers import GetAdminDetails
+from kaaya_login.models import TblKaayaLogin
+from kaaya_login.serializers import DeleteLoginDetails
+from rest_framework.pagination import PageNumberPagination
+
 
 class AllVendorView(APIView):
     authentication_classes = [JWTAuthentication]
@@ -44,7 +50,6 @@ class AllVendorView(APIView):
         success = False
         data = NULL
         message = ''
-        vendorData = {}
         responseCode = status.HTTP_500_INTERNAL_SERVER_ERROR
         # Approve vendor by admin
         try:
@@ -64,5 +69,97 @@ class AllVendorView(APIView):
         except Exception as e:
             return Response({'success':success, 'data':data, 'message':e}, status=responseCode)
         return Response({'success':success, 'data':data, 'message':message}, status=responseCode)
+    
+    def delete(self,request):
+        success = False
+        data = NULL
+        message = ''
+        responseCode = status.HTTP_500_INTERNAL_SERVER_ERROR
+        try:
+            if request.user.usertype == 1:
+                request.data._mutable = True
+                if int(request.GET.get('vendor_id')):
+                    try:
+                        request.data['is_deleted'] = 1
+                        request.data['is_active'] = 0
+                        request.data['modified_by'] = request.user.id
+                        request.data['modified_date'] = timezone.localtime(timezone.now())
+                        vendorInstance = TblKaayaVendorDetails.objects.filter(id=request.GET.get('vendor_id'))
+                        if vendorInstance:
+                            vendornUser = TblKaayaLogin.objects.filter(id = vendorInstance.user)
+                            deleteUser = DeleteLoginDetails(instance=vendornUser,data = request.data, partial =True)
+                            if deleteUser.is_valid() and deleteUser.save():
+                                vendorInstance.update(is_deleted = 1)
+                                success = True
+                                message = "Vendor deleted successfully"
+                                data = ""
+                                responseCode = status.HTTP_200_OK
+                            else:
+                                message = deleteUser.errors
+                    except TblKaayaAdminDetails.DoesNotExist:
+                        message = "Vendor doesn't exist"
+                request.data._mutable = False
+        except Exception as e:
+            return Response({'success':success, 'data':data, 'message':e}, status=responseCode)
+        return Response({'success':success, 'data':data, 'message':message}, status=responseCode)
 
 
+class AllAdminView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    # Get all the admin list
+    
+    def post(self,request):
+        success = False
+        data = NULL
+        message = ''
+        responseCode = status.HTTP_500_INTERNAL_SERVER_ERROR
+        # Admin can add another
+        try:
+            if request.user.usertype == 1:
+                request.data._mutable = True
+                addAdmin = GetAdminDetails(request.data,partial = True)
+                if addAdmin.is_valid() and addAdmin.save():
+                    success = True
+                    message = "Admin added succesfully"
+                    data = addAdmin.data
+                    responseCode = status.HTTP_201_CREATED
+                else:
+                    message = addAdmin.errors
+                request.data._mutable = False
+        except Exception as e:
+            return Response({'success':success, 'data':data, 'message':e}, status=responseCode)
+        return Response({'success':success, 'data':data, 'message':message}, status=responseCode)
+    
+    def delete(self,request):
+        success = False
+        data = NULL
+        message = ''
+        responseCode = status.HTTP_500_INTERNAL_SERVER_ERROR
+        # Admin can delete another admin
+        try:
+            if request.user.usertype == 1:
+                request.data._mutable = True
+                if int(request.GET.get('admin_id')) != 1:
+                    try:
+                        request.data['is_deleted'] = 1
+                        request.data['is_active'] = 0
+                        request.data['modified_by'] = request.user.id
+                        request.data['modified_date'] = timezone.localtime(timezone.now())
+                        adminInstance = TblKaayaAdminDetails.objects.filter(id=request.GET.get('admin_id'))
+                        if adminInstance:
+                            adminUser = TblKaayaLogin.objects.filter(id = adminInstance.user)
+                            deleteUser = DeleteLoginDetails(instance=adminUser,data = request.data, partial =True)
+                            if deleteUser.is_valid() and deleteUser.save():
+                                success = True
+                                message = "Admin deleted successfully"
+                                data = ""
+                                responseCode = status.HTTP_200_OK
+                            else:
+                                message = deleteUser.errors
+                    except TblKaayaAdminDetails.DoesNotExist:
+                        message = "Admin doesn't exist"
+                request.data._mutable = False
+        except Exception as e:
+            return Response({'success':success, 'data':data, 'message':e}, status=responseCode)
+        return Response({'success':success, 'data':data, 'message':message}, status=responseCode)
