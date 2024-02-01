6bbb97f437f37cb8a3b5105ac6ae39c96b47e57a
Karandeep Randhawa <karandeeprandhawa27@gmail.com>
2024-02-01 17:19:27 +0530

    Refresh Token API

--- kaaya_backend/urls.py
+++ kaaya_backend/urls.py
@@ -1,25 +1,11 @@
-"""kaaya_backend URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
-from kaaya_login.views import KaayaaLoginUser,Profile
+from kaaya_login.views import KaayaaLoginUser,Profile,Logout
 from kaaya_products.views import ProductsView,CategoryView
 from kaayaa_vendor.views import VendorRegisterView
 from kaayaa_admin.views import AllVendorView
 from kaayaa_user.views import UserRegisterView, CartOperations
+from kaaya_login.refreshToken import MyTokenObtainPairView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,6 +15,7 @@ urlpatterns = [
     path('category/', CategoryView.as_view()),
     path('register_vendor/', VendorRegisterView.as_view()),
     path('all_vendor_data/', AllVendorView.as_view()),
+    path('refreshToken/',MyTokenObtainPairView.as_view()),
     
     # User
     path('add-user/', UserRegisterView.as_view()),
